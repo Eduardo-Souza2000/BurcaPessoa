f3f1813f5ad667a57c40a71a0900ff1c90d519f2
Eduardo-Souza2000 <eduardosouza.foz2014@outlook.com>
2023-07-30 15:17:12 -0300

    iniciando o projeto

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        List<Pessoa> pessoas = new ArrayList<>();
+        List<Enderecos> enderecos = new ArrayList<>();
+
     }
 }
